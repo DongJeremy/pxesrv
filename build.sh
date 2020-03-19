599778ef4351b0ca293335482ab094b5284aeeb8
dongjeremy <dongjeremy2020@163.com>
2020-03-19 10:09:47 +0800

    change rpm folder.

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 set -x
 version=0.0.1
-git archive --format tar.gz --output ../v$version.tar.gz master --prefix pxesrv-$version/
+/usr/bin/git archive --format tar.gz --output /tmp/v$version.tar.gz master --prefix pxesrv-$version/
 
-/usr/bin/cp ../v$version.tar.gz /root/rpmbuild/SOURCES
-rpmbuild -ba pxesrv.spec
+/usr/bin/cp /tmp/v$version.tar.gz /root/rpmbuild/SOURCES
+/usr/bin/rpmbuild -ba pxesrv.spec
 
-mkdir -p ../dist
-/usr/bin/cp /root/rpmbuild/RPMS/x86_64/pxesrv*.rpm /root/rpmbuild/SRPMS/pxesrv* ../dist
+/usr/binmkdir -p /tmp/dist
+/usr/bin/cp /root/rpmbuild/RPMS/x86_64/pxesrv*.rpm /root/rpmbuild/SRPMS/pxesrv* /tmp/dist
 
 exit 0
