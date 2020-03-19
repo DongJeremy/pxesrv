3efb2f37a4cf9d286b30386f94f8c27a059e8b6c
dongjeremy <dongjeremy2020@163.com>
2020-03-19 10:15:36 +0800

    fix mistake bug.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ version=0.0.1
 /usr/bin/cp /tmp/v$version.tar.gz /root/rpmbuild/SOURCES
 /usr/bin/rpmbuild -ba pxesrv.spec
 
-/usr/binmkdir -p /tmp/dist
+/usr/bin/mkdir -p /tmp/dist
 /usr/bin/cp /root/rpmbuild/RPMS/x86_64/pxesrv*.rpm /root/rpmbuild/SRPMS/pxesrv* /tmp/dist
 
 exit 0
